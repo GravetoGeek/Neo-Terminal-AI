58246528a9c905d6756a9ff216bb8a999f8d0771
gravetogeek <edusrm11@gmail.com>
2025-01-04 14:32:51 -0300

    :ok_hand: feat: Inclusão da classe History

---
+++ src/modules/terminal/domain/entities/history.ts
@@ -0,0 +1,17 @@
+import {CommandResult} from './command-result'; // Adjust the path as necessary
+
+export class History {
+    constructor(
+        public readonly historyId: string,       // ID único do histórico
+        public readonly userId: string,          // ID do usuário (ou global se null)
+        public readonly commands: CommandResult[] // Lista de resultados de comandos
+    ) {}
+
+    addResult(result: CommandResult): void {
+        this.commands.push(result);
+    }
+
+    getLastExecuted(): CommandResult|null {
+        return this.commands.length>0? this.commands[this.commands.length-1]:null;
+    }
+}
