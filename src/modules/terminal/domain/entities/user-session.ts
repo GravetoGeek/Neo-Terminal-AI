06723306b6271a0ba0462d2d2bc2f63eb9f82608
gravetogeek <edusrm11@gmail.com>
2025-01-04 14:33:12 -0300

    :ok_hand: feat: Inclusão da classe UserSession

---
+++ src/modules/terminal/domain/entities/user-session.ts
@@ -0,0 +1,19 @@
+import {TerminalCommand} from './terminal-command';
+
+export class UserSession {
+    constructor(
+        public readonly sessionId: string, // ID único da sessão
+        public readonly userId: string,    // ID do usuário associado
+        public readonly commands: TerminalCommand[], // Histórico de comandos na sessão
+        public readonly startedAt: Date,   // Data/hora de início da sessão
+        public readonly expiredAt: Date|null, // Data/hora de expiração (se aplicável)
+    ) {}
+
+    addCommand(command: TerminalCommand): void {
+        this.commands.push(command);
+    }
+
+    clearHistory(): void {
+        this.commands.length=0;
+    }
+}
