45937e7dfe1bce8f8277f0e921d7281334c08c1f
gravetogeek <edusrm11@gmail.com>
2025-01-05 12:16:38 -0300

    :recycle: refactor: Renomeando entiddade  para corresponder ao nome da classe History

--- src/modules/terminal/domain/entities/History.ts
+++ src/modules/terminal/domain/entities/History.ts
@@ -7,10 +7,18 @@ export default class History {
         public readonly commands: CommandResult[] // Lista de resultados de comandos
     ) {}
 
+    /**
+     * Adiciona um resultado de comando ao histórico.
+     * @param result Resultado a ser adicionado.
+     */
     addResult(result: CommandResult): void {
         this.commands.push(result);
     }
 
+    /**
+     * Retorna o último comando executado.
+     * @returns Último comando executado.
+     */
     getLastExecuted(): CommandResult|null {
         return this.commands.length>0? this.commands[this.commands.length-1]:null;
     }
