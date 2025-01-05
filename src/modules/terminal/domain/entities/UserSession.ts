c4b26ee61d91d3583aa5d79c718a8b584426f4a3
gravetogeek <edusrm11@gmail.com>
2025-01-05 12:16:04 -0300

    :recycle: refactor: Renomeando entiddade  para corresponder ao nome da classe UserSession

--- src/modules/terminal/domain/entities/UserSession.ts
+++ src/modules/terminal/domain/entities/UserSession.ts
@@ -1,4 +1,5 @@
 import TerminalCommand from '@modules/terminal/domain/entities/TerminalCommand';
+import {stat} from 'fs';
 
 export class UserSession {
     constructor(
@@ -9,11 +10,66 @@ export class UserSession {
         public readonly expiredAt: Date|null, // Data/hora de expiração (se aplicável)
     ) {}
 
+    /**
+     * Adiciona um comando ao histórico da sessão.
+     * @param command Comando a ser adicionado.
+     * @returns void
+     */
     addCommand(command: TerminalCommand): void {
         this.commands.push(command);
     }
 
+    /**
+     * Limpa o histórico de comandos da sessão.
+     * @returns void
+     */
     clearHistory(): void {
         this.commands.length=0;
     }
+
+    /**
+     * Verifica se a sessão expirou.
+     * @returns boolean
+     */
+    isExpired(): boolean {
+        return this.expiredAt!==null&&this.expiredAt<new Date();
+    }
+
+    /**
+     * Verifica se a sessão está ativa.
+     * @returns boolean
+     */
+    hasCommand(commandName: string): boolean {
+        return this.commands.some((command) => command.name===commandName);
+    }
+
+    /**
+     * Retorna um comando do histórico da sessão.
+     * @param commandName Nome do comando a ser retornado.
+     * @returns TerminalCommand|undefined
+     */
+    getCommand(commandName: string): TerminalCommand|undefined {
+        return this.commands.find((command) => command.name===commandName);
+    }
+
+    /**
+     * Retorna o histórico de comandos da sessão.
+     * @returns TerminalCommand[]
+     */
+    getCommands(): TerminalCommand[] {
+        return this.commands;
+    }
+
+    /**
+     * Remove um comando do histórico da sessão.
+     * @param commandName Nome do comando a ser removido.
+     * @returns void
+     */
+    removeCommand(commandName: string): void {
+        const index=this.commands.findIndex((command) => command.name===commandName);
+        if(index!==-1) {
+            this.commands.splice(index,1);
+        }
+    }
+
 }
