f36ac538a8168210b4bb932f88dfd8867723b7a4
gravetogeek <edusrm11@gmail.com>
2025-01-05 12:16:21 -0300

    :recycle: refactor: Renomeando entiddade  para corresponder ao nome da classe Event

--- src/modules/terminal/domain/entities/Event.ts
+++ src/modules/terminal/domain/entities/Event.ts
@@ -2,7 +2,7 @@ export default class Event {
     constructor(
         public readonly eventId: string, // ID único do evento
         public readonly type: string,    // Tipo do evento (e.g., "commandExecuted", "errorOccurred")
-        public readonly payload: any,    // Dados associados ao evento
         public readonly occurredAt: Date, // Data/hora do evento
+        public readonly payload: Object={},    // Dados associados ao evento
     ) {}
 }
