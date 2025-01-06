a38f42aab193f5793b8fad1a63712219ab3709bf
gravetogeek <edusrm11@gmail.com>
2025-01-06 17:25:29 -0300

    :recycle: refactor: Alterando tipagem da propriedade payload da entidade Event

--- src/modules/terminal/domain/entities/Event.ts
+++ src/modules/terminal/domain/entities/Event.ts
@@ -3,6 +3,6 @@ export default class Event {
         public readonly eventId: string, // ID único do evento
         public readonly type: string,    // Tipo do evento (e.g., "commandExecuted", "errorOccurred")
         public readonly occurredAt: Date, // Data/hora do evento
-        public readonly payload: Object={},    // Dados associados ao evento
+        public readonly payload: object={},    // Dados associados ao evento
     ) {}
 }
